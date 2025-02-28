24448dcc68ccfd7d7beffe588cc8aa8cbda8a7b0
Police65 <65681291+Police65@users.noreply.github.com>
2025-02-28 13:03:37 -0400

    Fix in process

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+// import { useState } from 'react'
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import viteLogo from './supabase-client';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+     <div>{" "}
+      <h1> prueba </h1>
+     </div>
+<input type="text" placeholder='New todo'/>
+<button>add item</button>
+<ul>
+
+</ul>
+
+
+    </>
+  )
+}
+
+export default App
--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,5 @@
-    // src/supabaseClient.js
-    import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
-    const supabaseUrl = 'https://pckbdperupovxrniubrl.supabase.co';
-    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBja2JkcGVydXBvdnhybml1YnJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAxNDM1MTIsImV4cCI6MjA1NTcxOTUxMn0.p1qIe03QXpwmMKGeiobK-i5Sv_2ANYkSUQ-eUfydOis';
-
-    export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
