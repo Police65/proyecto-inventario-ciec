66f39dd6df7c100576e80c3f2021e40d03de9215
Police65 <65681291+Police65@users.noreply.github.com>
2025-06-18 14:54:57 -0400

    alfa

--- alfa version/src/supabaseClientTest.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = "https://pckbdperupovxrniubrl.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBja2JkcGVydXBvdnhybml1YnJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAxNDM1MTIsImV4cCI6MjA1NTcxOTUxMn0.p1qIe03QXpwmMKGeiobK-i5Sv_2ANYkSUQ-eUfydOis";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
